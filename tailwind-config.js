63203c3566f4b323bbbf2a375d9178b263217c28
nayan das <nayandas559@gmail.com>
2023-01-26 23:18:58 +0600

    first commit

---
+++ tailwind-config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        yellow: "#FECC02",
+        black: "#131313",
+        gray: "#4E4E4E",
+      },
+      fontFamily: {
+        bebas: ["Bebas Neue", "cursive"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
+  },
+};
